dbea2a2869ccbe170750a94b485aaa691b469e97
Nathan Ridley <axefrog@gmail.com>
2016-03-23 14:33:42 +1000

    Correctly set boolean attributes

--- modules/attributes.js
+++ modules/attributes.js
@@ -1,29 +1,30 @@
-var booleanAttrs = ["allowfullscreen", "async", "autofocus", "autoplay", "checked", "compact", "controls", "declare", 
-                "default", "defaultchecked", "defaultmuted", "defaultselected", "defer", "disabled", "draggable", 
-                "enabled", "formnovalidate", "hidden", "indeterminate", "inert", "ismap", "itemscope", "loop", "multiple", 
-                "muted", "nohref", "noresize", "noshade", "novalidate", "nowrap", "open", "pauseonexit", "readonly", 
-                "required", "reversed", "scoped", "seamless", "selected", "sortable", "spellcheck", "translate", 
+var booleanAttrs = ["allowfullscreen", "async", "autofocus", "autoplay", "checked", "compact", "controls", "declare",
+                "default", "defaultchecked", "defaultmuted", "defaultselected", "defer", "disabled", "draggable",
+                "enabled", "formnovalidate", "hidden", "indeterminate", "inert", "ismap", "itemscope", "loop", "multiple",
+                "muted", "nohref", "noresize", "noshade", "novalidate", "nowrap", "open", "pauseonexit", "readonly",
+                "required", "reversed", "scoped", "seamless", "selected", "sortable", "spellcheck", "translate",
                 "truespeed", "typemustmatch", "visible"];
-    
+
 var booleanAttrsDict = {};
 for(var i=0, len = booleanAttrs.length; i < len; i++) {
   booleanAttrsDict[booleanAttrs[i]] = true;
 }
-    
+
 function updateAttrs(oldVnode, vnode) {
   var key, cur, old, elm = vnode.elm,
       oldAttrs = oldVnode.data.attrs || {}, attrs = vnode.data.attrs || {};
-  
+
   // update modified attributes, add new attributes
   for (key in attrs) {
     cur = attrs[key];
     old = oldAttrs[key];
     if (old !== cur) {
       // TODO: add support to namespaced attributes (setAttributeNS)
-      if(!cur && booleanAttrsDict[key])
+      var isBooleanAttribute = booleanAttrsDict[key];
+      if(!cur && isBooleanAttribute)
         elm.removeAttribute(key);
       else
-        elm.setAttribute(key, cur);
+        elm.setAttribute(key, isBooleanAttribute ? '' : cur);
     }
   }
   //remove removed attributes
